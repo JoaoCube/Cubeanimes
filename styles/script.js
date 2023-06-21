3dfac829bd61c331640dd0d884def4de058c0715
JoaoCube <victorbatistame@gmail.com>
2023-06-20 20:05:18 -0400

    mudanca responsivo e page anime

--- styles/script.js
+++ styles/script.js
@@ -26,7 +26,7 @@ window.onload = function () {
     popup.style.opacity = "1";
 
     // ADICIONAR ALGUNS SEGUNDOS DE DELAY PRA MOSTRAR O POP UP
-  }, 60000);
+  }, 100000);
 };
 // FECHAR POP UP AO CLICAR NO X
 close.addEventListener("click", () => {
