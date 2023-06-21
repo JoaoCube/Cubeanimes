6fa7d4e3400acf43de3a531734ae1424d0b7a265
JoaoCube <victorbatistame@gmail.com>
2023-06-20 20:52:38 -0400

    mudanca about responsive

--- styles/script.js
+++ styles/script.js
@@ -81,3 +81,12 @@ function openModal() {
     }
   });
 }
+
+function toggleP() {
+  let p = document.getElementById("resposta");
+  if (p.style.display === "none") {
+    p.style.display = "block";
+  } else {
+    p.style.display = "none";
+  }
+}
