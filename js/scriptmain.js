abd47a6e3696bbec60c955fc67261de213f13a78
JoaoCube <victorbatistame@gmail.com>
2023-08-15 02:42:16 -0400

    slider e otimizacao

--- js/scriptmain.js
+++ js/scriptmain.js
@@ -1,28 +1,11 @@
 /*
-//CUIDANDO DA ANIMACAO DO BANNER TOP-SITE
-let count = 1;
-document.getElementById("radio1").checked = true;
-setInterval(function () {
-  nextImage();
-}, 10000);
-
-function nextImage() {
-  count++;
-  if (count > 5) {
-    count = 1;
-  }
-
-  document.getElementById("radio" + count).checked = true;
-}
-*/
-
 // Lista de imagens
 const imagens = [
-  "img-op.png",
-  "img-zom100.jpg",
-  "img-jujutsu.png",
-  "img-Undead-Murder-Farce.jpg",
-  "img-mobpsycho.jpg",
+  "/publics/imgs/img-op.png",
+  "/publics/imgs/img-zom100.jpg",
+  "/publics/imgs/img-jujutsu.png",
+  "/publics/imgs/img-Undead-Murder-Farce.jpg",
+  "/publics/imgs/img-mobpsycho.jpg",
 ];
 
 // Função para escolher uma imagem aleatória
@@ -37,3 +20,35 @@ window.addEventListener("load", () => {
   let novaImagemSrc = escolherImagemAleatoria();
   imagemElemento.src = novaImagemSrc;
 });
+*/
+
+const slider = document.querySelector(".slider");
+const slides = slider.querySelectorAll(".slide");
+
+let currentIndex = 0;
+
+function showImage(index) {
+  slides.forEach((image, i) => {
+    if (i === index) {
+      image.style.display = "block";
+    } else {
+      image.style.display = "none";
+    }
+  });
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showImage(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
+
+// imagem inicial
+showImage(currentIndex);
+
+// muda slide a cada x segundos
+setInterval(nextSlide, 20000);
