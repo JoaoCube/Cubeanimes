7de806c5c12608c29c89c4f3812cf3bea6b5274b
JoaoCube <victorbatistame@gmail.com>
2023-08-16 15:49:37 -0400

    area de pagamento e responsividade

--- js/planos.js
+++ js/planos.js
@@ -1,9 +1,7 @@
 // Obtém a lista de itens
 const listaItens = document.getElementById("listaPass");
-
 // Obtém todos os itens da lista
 const itens = listaItens.getElementsByTagName("li");
-
 // Adiciona um ouvinte de evento de clique a cada item
 for (let i = 0; i < itens.length; i++) {
   itens[i].addEventListener("click", function () {
@@ -17,39 +15,36 @@ for (let i = 0; i < itens.length; i++) {
   });
 }
 
-// Obtém o botão e o container
-const trocarDivBtn = document.getElementById("trocarDivBtn");
-const container = document.getElementById("elementoAlvo");
+// VOLTAR ETAPA E AVANCAR, ADICIONANDO E REMOVENDO CLASSE
+const voltarEtapaBtn = document.getElementById("voltarEtapaBtn");
+const etapa1 = document.getElementById("etapa1");
+const etapa2 = document.getElementById("etapa2");
+const avancaEtapa1 = document.getElementById("avancaEtapa1");
+const avancaEtapa2 = document.getElementById("avancaEtapa2");
 
-// Adiciona um ouvinte de evento de clique ao botão
-trocarDivBtn.addEventListener("click", function () {
-  // Remove a div existente
-  const div1 = document.getElementById("listaPass");
-  container.removeChild(div1);
+// Controla o estado da página
+let etapaAtual = 1;
 
-  // Cria uma nova div
-  const div2 = document.createElement("div");
-  div2.id = "pagamento";
-  div2.innerHTML = `
-  <article class="pay-container" id="pagamento">
-  <div class="pay-conteudo">
-      <ul class=' cor-8 font-2-mR'>
-      <li>
-      <input type="radio" name="my-input" id="pix" value="Pix" />
-      <label for="Yes">Pix</label>
-      </li>
-      <li>
-      <input type="radio" name="my-input" id="boleto" value="Boleto" />
-      <label for="Boleto">Boleto</label>
-      </li>
-      <li>
-      <input type="radio" name="my-input" id="cartao" value="Boleto" />
-      <label for="Cartao de Credito">Cartao de Credito</label>
-      </li>
-      </ul>
-  </div>
-</article>`;
+// Botão para avançar da etapa 1 para a etapa 2
+avancaEtapa1.addEventListener("click", function () {
+  etapa1.style.display = "none";
+  etapa2.style.display = "block";
+  etapaAtual = 2;
+});
 
-  // Adiciona a nova div ao container
-  container.appendChild(div2);
+/*
+// Botão para avançar da etapa 2 para a etapa 3
+avancaEtapa2.addEventListener("click", function () {
+  // Implemente a lógica da etapa 2 para etapa 3 aqui
+});
+*/
+
+// Botão para voltar uma etapa
+voltarEtapaBtn.addEventListener("click", function () {
+  if (etapaAtual === 2) {
+    etapa2.style.display = "none";
+    etapa1.style.display = "block";
+    etapaAtual = 1;
+  }
+  // Implemente a lógica para voltar da etapa 3 para etapa 2 aqui
 });
